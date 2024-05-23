43810f9493adf2555754b9120a41c06daad1fb69
jrcdizumi <cd15382858999@163.com>
2024-05-24 01:26:42 +0800

    #

--- hotel/models.py
+++ hotel/models.py
@@ -4,6 +4,8 @@ import threading
 import django
 import csv
 from django.core.exceptions import ObjectDoesNotExist
+from datetime import datetime
+from datetime import timedelta
 from django.db.models import Q
 
 # Create your models here.
@@ -11,13 +13,13 @@ class Room(models.Model):
     FAN_SPEED = [
         (3, "HIGH"),
         (2, "MIDDLE"),
-        (1, "LOW"),
+        (1, "LOW")
     ]
 
     room_id = models.IntegerField(verbose_name='房间号', default=1, primary_key=True)
 
     # 入住时间
-    request_time = models.DateTimeField(verbose_name='入住时间', default=timezone.now)
+    start_time = models.DateTimeField(verbose_name='入住时间', default=timezone.now)
 
     # 当前温度
     current_temp = models.FloatField(verbose_name="当前温度", default=25.0)
@@ -67,6 +69,7 @@ class Room(models.Model):
     # 入住时调用该函数，将房间设置为有人入住状态
     def checkin(self):
         self.is_occupied = True
+        start_time = timezone.now()
         self.save()
 
     # 退房时调用该函数，返回当前费用、自动关闭空调并将费用清零
@@ -103,24 +106,48 @@ class Room(models.Model):
     def get_room(room_id):
         return Room.objects.get(room_id=room_id)
 
+    #创建n个房间，如果数据库中已经有房间，则不会创建
+    @staticmethod
+    def create_room(n=6):
+        for i in range(1, n + 1):
+            try:
+                Room.objects.get(room_id=i)
+            except ObjectDoesNotExist:
+                Room.objects.create(room_id=i)
+
 # 用户的所有请求，包括温度调整、风速调整、开关机、入住退房等
 class Request(models.Model):
+    REQUEST_CHOICE = [
+        (0, 'turn on/off'),
+        (1, 'increase temp'),
+        (2, 'decrease temp'),
+        (3, 'increase speed'),
+        (4, 'decrease speed')
+    ]
+    FAN_SPEED = [
+        (3, "HIGH"),
+        (2, "MIDDLE"),
+        (1, "LOW")
+    ]
 
     request_time = models.DateTimeField(verbose_name='请求时间', default=timezone.now)
 
-    room_id = models.IntegerField(verbose_name='房间号', default=0)
+    room_id = models.IntegerField(verbose_name='房间号', default=1)
+
+    # 入住时间，和room_id一起区分不同客人
+    start_time = models.DateTimeField(verbose_name='入住时间', default=timezone.now)
 
     # 0: turn on/off; 1: increase temp; 2: decrease temp; 3: increase speed; 4: decrease speed
-    request_type = models.IntegerField(verbose_name='请求类型', default=0)
+    request_type = models.IntegerField(verbose_name='请求类型', default=0, choices=REQUEST_CHOICE)
 
     # 请求后的温度
-    current_temp = models.FloatField(verbose_name='目标温度', default=25.0)
+    current_temp = models.FloatField(verbose_name='温度', default=25.0)
 
     # 请求后的风速
-    fan_speed = models.IntegerField(verbose_name='目标风速', default=2)
+    fan_speed = models.IntegerField(verbose_name='风速', default=2, choices=FAN_SPEED)
 
     # 请求后的费用
-    fee = models.FloatField(verbose_name='目标费用', default=0.0)
+    fee = models.FloatField(verbose_name='费用', default=0.0)
 
     # 请求后的费率
     fee_rate = models.FloatField(verbose_name='当前费率', default=0.3)
@@ -128,20 +155,51 @@ class Request(models.Model):
     # 请求后空调是否开启
     on = models.BooleanField(verbose_name='空调是否开启', default=False)
 
-    # 根据room_id获取对应房间
-    room = Room.get_room(room_id)
+    # 根据房间号获取温度、风速、费用、费率以及空调状态并写入数据库
+    def write(self):
+        room = Room.get_room(self.room_id)
+        self.start_time = room.start_time
+        self.current_temp = room.current_temp
+        self.fan_speed = room.fan_speed
+        self.fee = room.fee
+        self.fee_rate = room.fee_rate
+        self.on = room.on
+        self.save()
 
-    current_temp = room.current_temp
+    # 根据请求类型处理请求并写入数据库
+    def process(self):
+        room = Room.get_room(self.room_id)
+        if self.request_type == 0:
+            if self.on:
+                room.turn_off()
+            else:
+                room.turn_on()
+        elif self.request_type == 1:
+            room.increase_temp()
+        elif self.request_type == 2:
+            room.decrease_temp()
+        elif self.request_type == 3:
+            room.increase_speed()
+        elif self.request_type == 4:
+            room.decrease_speed()
+        self.write()
 
-    fan_speed = room.fan_speed
+    # 根据房间号、入住时间和请求时间的区间获取请求列表
+    @staticmethod
+    def get_request(room_id=None, start_time=None, left_time=None, right_time=None):
+        # 如果没有传入right_time,则默认为当前时间
+        if right_time is None:
+            right_time = timezone.now()
+        # 如果没有传入left_time,则默认为right_time的前三天
+        if left_time is None:
+            left_time = right_time - timedelta(days=3)
+        if room_id is None:
+            return Request.objects.filter(request_time__range=(left_time, right_time))
+        if start_time is None:
+            return Request.objects.filter(room_id=room_id, request_time__range=(left_time, right_time))
+        return Request.objects.filter(room_id=room_id, start_time=start_time, request_time__range=(left_time, right_time))
 
-    fee = room.fee
-
-    fee_rate = room.fee_rate
-
-    on = room.on
-
-
-
-
-
+    #将年、月、日、时、分、秒格式化
+    @staticmethod
+    def time_to_timestamp(year, month, day, hour=0, minute=0, second=0):
+        return datetime(year, month, day, hour, minute, second)
