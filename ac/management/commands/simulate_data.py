354c733b48022d49fd6e8184ea2cc55600122557
jrcdizumi <cd15382858999@163.com>
2024-05-24 17:28:56 +0800

    #

--- ac/management/commands/simulate_data.py
+++ ac/management/commands/simulate_data.py
@@ -3,6 +3,7 @@ from hotel.models import Room
 import random
 import time
 
+
 class Command(BaseCommand):
     help = 'Simulate data changes in Room model'
 
@@ -10,10 +11,10 @@ class Command(BaseCommand):
         fan_speeds = {'Low': 1, 'Medium': 2, 'High': 3}
         while True:
             for room in Room.objects.all():
-                room.current_temp = random.randint(16.0, 30.0)  # Random current_temp between 16.0 and 30.0
+                room.current_temp = random.randint(16, 30)  # Random current_temp between 16.0 and 30.0
                 room.fan_speed =random.randint(1, 3)
-                room.fee += round(random.uniform(0.0, 5.0),2)  # Randomly increase fee
+                room.fee += round(random.uniform(0.0, 5.0), 2)  # Randomly increase fee
                 room.on = random.choice([True, False])  # Random on status
-                room.fee_rate = round(random.uniform(0.0, 5.0),2)  # Random fee_rate between 0.0 and 5.0
+                room.fee_rate = round(random.uniform(0.0, 5.0), 2)  # Random fee_rate between 0.0 and 5.0
                 room.save()
             time.sleep(3)
