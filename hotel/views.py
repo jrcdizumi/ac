997f5f98f9a23def9cabc4f76a3a1217fda12071
Akarin <1791587832@qq.com>
2024-05-25 02:02:41 +0800

    tmp

--- hotel/views.py
+++ hotel/views.py
@@ -54,3 +54,85 @@ def check_out(request):
     req_list = json.loads(serialize('json', request.get_request(room_id, request.start_time,request.start_time)))
     ret = [[{'room_id' : room_id, 'fee' : fee}], req_list]
     return JsonResponse(ret, safe=False)
+
+
+
+# ============静态变量===========
+
+class RoomCounter:  # 分配房间号
+    num = 0
+    dic = {}
+
+
+class RoomInfo:  # Room->字典
+    dic = {
+        "target_temp": "--",
+        "init_temp": "--",
+        "current_temp": "--",
+        "fan_speed": "--",
+        "fee": 0,
+        "room_id": 0
+    }
+
+    def __init__(self, room):
+        self.dic["target_temp"] = room.target_temp
+        self.dic["init_temp"] = room.init_temp
+        self.dic["current_temp"] = int(room.current_temp)
+        self.dic["fan_speed"] = speed_ch[room.fan_speed]
+        self.dic["fee"] = int(room.fee)
+        self.dic["room_id"] = room.room_id
+class RoomBuffer:  # 房间数据缓存
+    on_flag = [None, False, False, False, False, False]
+    target_temp = [0, 25, 25, 25, 25, 25]  # 不要用数组。。。。
+    init_temp = [0, 32, 28, 30, 29, 35]
+
+room_id = RoomCounter  # 房间号与session_id对应
+room_info = RoomInfo # Room->字典
+room_data = RoomBuffer # 房间数据缓存
+speed_ch = ["", "高速", "中速", "低速"]
+state_ch = ["", "服务中", "等待", "关机", "休眠"]
+
+class RoomNumberExceededError(Exception):
+    """Raised when the room number exceeds the limit"""
+    pass
+
+
+#====================
+def get_room_id(request):
+    s_id = request.session.session_key
+    if s_id is None:
+        request.session.create()
+        s_id = request.session.session_key
+
+    if s_id not in room_id.dic:
+        room_id.num += 1
+        if room_id.num > 5:
+            raise RoomNumberExceededError("Room number has exceeded the limit")
+        else:
+            room_id.dic[s_id] = room_id.num
+            return room_id.dic[s_id]
+    else:
+        return room_id.dic[s_id]
+
+def client_off(request):  # 第一次访问客户端界面、# 开机
+    room_id = get_room_id(request)
+    room = Room.get_room(room_id)
+    if room.on:
+        room.turn_off()
+
+
+def client_on(request):
+
+def power(request):  # 客户端-电源键
+
+def change_high(request):  # 客户端-高风
+
+def change_mid(request):  # 中速
+
+def change_low(request):  # 低速
+
+def change_up(request):  # 升高温度
+
+def change_down(request):  # 降低温度
+
+#====================
