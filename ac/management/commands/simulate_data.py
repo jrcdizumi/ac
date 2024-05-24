3b8481058cf4f4950bd3a37a536cfe795ddff25d
lgzzgl <2749801299@qq.com>
2024-05-24 17:46:47 +0800

    #

--- ac/management/commands/simulate_data.py
+++ ac/management/commands/simulate_data.py
@@ -10,7 +10,7 @@ class Command(BaseCommand):
         fan_speeds = {'Low': 1, 'Medium': 2, 'High': 3}
         while True:
             for room in Room.objects.all():
-                room.current_temp = random.randint(16.0, 30.0)  # Random current_temp between 16.0 and 30.0
+                room.current_temp = random.randint(16, 30)  # Random current_temp between 16.0 and 30.0
                 room.fan_speed =random.randint(1, 3)
                 room.fee += round(random.uniform(0.0, 5.0),2)  # Randomly increase fee
                 room.on = random.choice([True, False])  # Random on status
