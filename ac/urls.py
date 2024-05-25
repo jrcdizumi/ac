8f6aa4647e237302300144d8bcd6bd8402f7be10
lgzzgl <2749801299@qq.com>
2024-05-25 14:51:29 +0800

    #监控管理

--- ac/urls.py
+++ ac/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('monitor/', get_monitor_page),
     path('refresh_monitor/', refresh_monitor, name='refresh_monitor'),
+    path('monitor_manage/', monitor_manage, name='monitor_manage'),
     
     path('front/', get_front_page),
     path('check_in/', check_in),
